f02513cecd030e389aebd7f18730e34fbbd5bc78
isnancahyadi <isnan.arifc@gmail.com>
2023-12-09 20:26:55 +0700

    [ADD] show more data

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from "@/components";
 import { fuels, yearsOfProduction } from "@/constants";
 import { fetchCars } from "@/utils";
 
@@ -42,6 +42,10 @@ export default async function Home({ searchParams }) {
             <p>{allCars?.message}</p>
           </div>
         )}
+        <ShowMore
+          pageNumber={(searchParams.limit || 10) / 10}
+          isNext={(searchParams.limit || 10) > allCars.length}
+        />
       </div>
     </main>
   );
